13010604c4d411971d3b6f7f0ace7f07face71a9
jing1127 <1078127666@qq.com>
2018-05-10 17:36:48 +0800

    c2c项目

---
+++ jedate/date.js
@@ -0,0 +1,23 @@
+/**
+ @Name : jeDate v3.8.5 日期控件
+ @Author: chen guojun
+ @Date: 2017-06-18
+ @QQ群：516754269
+ @官网：http://www.jemui.com/ 或 https://github.com/singod/jeDate
+ */
+
+$("#OPEN_DATE1").jeDate({
+    isinitVal:false,
+    festival:true,
+    ishmsVal:false,
+    format:"YYYY-MM-DD hh:mm:ss",
+    zIndex:3000,
+	})
+
+$("#OPEN_DATE2").jeDate({
+    isinitVal:false,
+    festival:true,
+    ishmsVal:false,
+    format:"YYYY-MM-DD hh:mm:ss",
+    zIndex:3000,
+	})
---
+++ js/close.js
@@ -0,0 +1,16 @@
+$("#clos").click(function(){
+					$(".mt").css("display","none");
+				})
+				$("#clos1").click(function(){
+					$(".mt").css("display","none");
+				})
+
+$("#clos2").click(function(){
+					$(".mt1").css("display","none");
+				})
+ $(".query_btn").mousedown(function(){
+	         	$(this).css({'background-color':'#e02d2d'});
+	         })
+	         $(".query_btn").mouseup(function(){
+	         	$(this).css({'background-color':'#fe3636'});
+	         })				
